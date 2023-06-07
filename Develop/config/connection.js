56ab2c9b70806b18a61ff5c249c93dcb57075fe1
Alcantn1 <nalcantara92@gmail.com>
2023-06-07 16:18:57 -0700

    fixed code

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -4,12 +4,21 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('process.env.ecommerce_db', 'process.env.alcantn1', 'process.env.Neeknasty69!', {
-    host: 'localhost',
+  : new Sequelize('ecommerce_db', 'root', 'Neeknasty69!', {
+      host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
